b958d8df375df05c665140ab7130f0266831c5bc
DmitryP0l <dmitrypoletaeu@gmail.com>
2022-09-30 23:06:35 +0300

    commit
    add podfile MarqueeLabel

---
+++ Pods/Target Support Files/Pods-imusicYndx/Pods-imusicYndx-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_imusicYndx : NSObject
+@end
+@implementation PodsDummy_Pods_imusicYndx
+@end
--- imusicYndx/ViewController.swift
+++ imusicYndx/ViewController.swift
@@ -14,6 +14,7 @@
 
 import UIKit
 import AVKit
+import MarqueeLabel
 
 final class ViewController: UIViewController {
     
